ce1aefe02a27c72f7557026027b6ad81fcb1a346
gabrielmissio <gabriel.missio@compasso.com.br>
2023-10-10 21:13:19 -0300

    Adicionar "MeuPrimeiroContrato" ao material "001-indroducao-ao-solidity"

---
+++ material/001-introducao-ao-solidity/contratos /MeuPrimeiroContrato.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MeuPrimeiroContrato {
+    function olaMundo() public pure returns (string memory) {
+        return "Ola, mundo!";
+    }
+}
